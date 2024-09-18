0c4826e6ef2d61a9acf697b050776d875cd8215e
AnaLuizaRChiamenti <analuizarodrigueschiamenti@gmail.com>
2024-09-18 11:18:20 -0300

    feat: Foi criado o header

---
+++ src/pages/utils/nav-buttons.ts
@@ -0,0 +1,8 @@
+export const navButtons = [
+    { id: 'novidades', text: 'NOVIDADES' },
+    { id: 'sapatos', text: 'SAPATOS' },
+    { id: 'botas', text: 'BOTAS' },
+    { id: 'bolsas', text: 'BOLSAS' },
+    { id: 'acessorios', text: 'ACESSÓRIOS' },
+    { id: 'promocoes', text: 'PROMOÇÕES' },
+];
