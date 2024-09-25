cb574ceddc63b130a300fe687cc79f70cfde7835
AnaLuizaRChiamenti <analuizarodrigueschiamenti@gmail.com>
2024-09-25 12:03:27 -0300

    feat: criado a parte de filtro e titulo da home de pdc

--- src/pages/novidades/index.tsx
+++ src/pages/novidades/index.tsx
@@ -1,3 +1,30 @@
-export default function homePDC() {
-  return <h1>Oi</h1>;
+import Image from 'next/image';
+import icon_filter from '/public/Images/Icones/icon-filter.png';
+
+export default function HomePDC() {
+  return (
+    <section className="flex items-center justify-center py-6 md:justify-start md:px-10 lg:px-16">
+      <button className="flex items-center justify-center px-7 py-2 border-2 border-gray-400 rounded-full hover:bg-gray-300 transform bg-transparent md:w-36 lg:w-48 lg:h-16 xl:w-36 xl:h-12">
+        <Image
+          src={icon_filter}
+          alt="Ícone de filtrar"
+          width={20}
+          className="mr-2 md:w-6 lg:w-8 xl:w-5"
+        />
+        <span className="text-gray-700 md:text-lg lg:text-2xl xl:text-lg">
+          Filtrar
+        </span>
+      </button>
+
+      <div className="px-10">
+        <h3 className="uppercase tracking-widest md:text-lg lg:text-2xl xl:text-2xl font-sans">
+          Sandálias
+        </h3>
+        <p className="hidden md:flex md:tracking-widest md:text-base lg:text-xl xl:text-sm font-sans text-gray-400">
+          Peça-chave do guarda-roupa, as sandálias femininas da Arezzo traduzem
+          e valorizam o estilo da mulher contemporânea.{' '}
+        </p>
+      </div>
+    </section>
+  );
 }
