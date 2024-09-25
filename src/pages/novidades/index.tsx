0cf9825218cb398ccb88569d4715139ae7198e7c
AnaLuizaRChiamenti <analuizarodrigueschiamenti@gmail.com>
2024-09-25 15:35:33 -0300

    feat: foi feita a logica do filtro

--- src/pages/novidades/index.tsx
+++ src/pages/novidades/index.tsx
@@ -1,10 +1,21 @@
 import Image from 'next/image';
 import icon_filter from '/public/Images/Icones/icon-filter.png';
+import { useState } from 'react';
+import Filter from '@/components/Filter';
 
 export default function HomePDC() {
+  const [isFilterVisible, setIsFilterVisible] = useState(false);
+
+  const toggleFilter = () => {
+    setIsFilterVisible(!isFilterVisible);
+  };
+
   return (
-    <section className="flex items-center justify-center py-6 md:justify-start md:px-10 lg:px-16">
-      <button className="flex items-center justify-center px-7 py-2 border-2 border-gray-400 rounded-full hover:bg-gray-300 transform bg-transparent md:w-36 lg:w-48 lg:h-16 xl:w-36 xl:h-12">
+    <section className="relative flex items-center justify-center py-6 md:justify-start md:px-10 lg:px-16">
+      <button
+        onClick={toggleFilter}
+        className="flex items-center justify-center px-7 py-2 border-2 border-gray-400 rounded-full hover:bg-gray-300 transform bg-transparent md:w-36 lg:w-48 lg:h-16 xl:w-36 xl:h-12"
+      >
         <Image
           src={icon_filter}
           alt="Ícone de filtrar"
@@ -25,6 +36,14 @@ export default function HomePDC() {
           e valorizam o estilo da mulher contemporânea.{' '}
         </p>
       </div>
+
+      {isFilterVisible && (
+        <>
+          <div className="fixed left-0 top-52 h-screen w-96 bg-white z-10">
+            <Filter onClose={toggleFilter} />
+          </div>
+        </>
+      )}
     </section>
   );
 }
