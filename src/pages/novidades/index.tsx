a35f587c8135c131d078bfcf01a422a01b41735f
AnaLuizaRChiamenti <analuizarodrigueschiamenti@gmail.com>
2024-09-26 14:24:25 -0300

    feat: foi ajustado alguns componentes que foram deslocados após merjar tudo

--- src/pages/novidades/index.tsx
+++ src/pages/novidades/index.tsx
@@ -26,36 +26,39 @@ export default function HomePDC({
   };
 
   return (
-    <section className="">
+    <section className="w-full">
       <div className="w-full pb-5 mb-5">
         <NavigationLane />
       </div>
       <div className="w-full">
         <CardNavList products={products} />
       </div>
-      <button
-        onClick={toggleFilter}
-        className="flex items-center justify-center px-7 py-2 border-2 border-gray-400 rounded-full hover:bg-gray-300 transform bg-transparent md:w-36 lg:w-48 lg:h-16 xl:w-36 xl:h-12"
-      >
-        <Image
-          src={icon_filter}
-          alt="Ícone de filtrar"
-          width={20}
-          className="mr-2 md:w-6 lg:w-8 xl:w-5"
-        />
-        <span className="text-gray-700 md:text-lg lg:text-2xl xl:text-lg">
-          Filtrar
-        </span>
-      </button>
 
-      <div className="px-10">
-        <h3 className="uppercase tracking-widest md:text-lg lg:text-2xl xl:text-2xl font-sans">
-          Sandálias
-        </h3>
-        <p className="hidden md:flex md:tracking-widest md:text-base lg:text-xl xl:text-sm font-sans text-gray-400">
-          Peça-chave do guarda-roupa, as sandálias femininas da Arezzo traduzem
-          e valorizam o estilo da mulher contemporânea.{' '}
-        </p>
+      <div className="flex items-center">
+        <button
+          onClick={toggleFilter}
+          className="flex ml-5 mt-5 items-center justify-center px-7 py-2 border-2 border-gray-400 rounded-full hover:bg-gray-300 transform bg-transparent md:w-36 lg:w-48 lg:h-16 xl:w-36 xl:h-12"
+        >
+          <Image
+            src={icon_filter}
+            alt="Ícone de filtrar"
+            width={20}
+            className="mr-2 md:w-6 lg:w-8 xl:w-5"
+          />
+          <span className="text-gray-700 md:text-lg lg:text-2xl xl:text-lg">
+            Filtrar
+          </span>
+        </button>
+
+        <div className="px-10 mt-4">
+          <h3 className="uppercase tracking-widest text-xl md:text-lg lg:text-2xl xl:text-2xl font-sans">
+            Sandálias
+          </h3>
+          <p className="hidden md:flex md:tracking-widest md:text-base lg:text-xl xl:text-sm font-sans text-gray-400">
+            Peça-chave do guarda-roupa, as sandálias femininas da Arezzo
+            traduzem e valorizam o estilo da mulher contemporânea.{' '}
+          </p>
+        </div>
       </div>
 
       <div>
@@ -69,7 +72,7 @@ export default function HomePDC({
 
       {isFilterVisible && (
         <>
-          <div className="fixed left-0 top-52 h-screen w-96 bg-white z-10">
+          <div className="fixed flex items-center left-0 top-0 h-screen w-96 bg-white z-10">
             <Filter onClose={toggleFilter} />
           </div>
         </>
