5811146283195149b2daac95d53361bcfe6527bb
AnaLuizaRChiamenti <analuizarodrigueschiamenti@gmail.com>
2024-09-27 16:43:04 -0300

    feat: ajustes feitos conforme o solicitado

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,6 @@
 import { Product } from '@/types/product';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 interface ProductCardProps {
   product: Product;
@@ -9,35 +9,29 @@ interface ProductCardProps {
 const renderProductImage = (
   images: { url: string }[],
   alt: string,
-  onClick: () => void
+  code: string
 ) => {
   if (images.length === 0) {
     return <p>Imagem não disponível</p>;
   }
 
   return (
-    <button onClick={onClick}>
+    <Link href={`/novidades/${code}`}>
       <Image
         src={images[0].url}
         alt={alt}
         layout="fill"
         className="object-cover"
       />
-    </button>
+    </Link>
   );
 };
 
 export default function ProductCard({ product }: ProductCardProps) {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push(`/novidades/${product.code}`);
-  };
-
   return (
     <div className="flex flex-col items-center min-w-44 lg:min-w-64 2xl:min-w-96 h-auto">
       <div className="relative w-full aspect-[4/5] mb-2 flex-grow">
-        {renderProductImage(product.images, product.name, handleClick)}
+        {renderProductImage(product.images, product.name, product.code)}
       </div>
       <div className="w-full flex items-center px-2">
         <h2 className="text-sm truncate w-2/3">{product.name}</h2>
