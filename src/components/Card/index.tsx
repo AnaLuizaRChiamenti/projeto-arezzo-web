a36fcf35de1787fc7af5b4a9a247136717c2f81d
AnaLuizaRChiamenti <analuizarodrigueschiamenti@gmail.com>
2024-10-01 14:18:42 -0300

    feat: ajuste em layout de pdc

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -29,12 +29,12 @@ const renderProductImage = (
 
 export default function ProductCard({ product }: ProductCardProps) {
   return (
-    <div className="flex flex-col items-center min-w-44 lg:min-w-64 2xl:min-w-96 h-auto">
-      <div className="relative w-full aspect-[4/5] mb-2 flex-grow">
+    <div className="flex flex-col mb-8 items-center min-w-44 lg:min-w-64 2xl:min-w-96">
+      <div className="relative w-full aspect-[4/5] flex-grow">
         {renderProductImage(product.images, product.name, product.code)}
       </div>
-      <div className="w-full flex items-center px-2">
-        <h2 className="text-sm truncate w-2/3">{product.name}</h2>
+      <div className="w-full flex items-center px-4 mt-4">
+        <h2 className="text-sm truncate w-2/3 tracking-wide">{product.name}</h2>
         <p className="text-sm font-bold truncate w-1/3 text-right">
           {product.price.formattedValue}
         </p>
